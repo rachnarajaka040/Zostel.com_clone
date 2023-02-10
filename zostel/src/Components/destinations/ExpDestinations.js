05cbcf9a4ca387ce4bf3324ecd920b22cc9a4f96
krishnapratap98 <117529882+krishnapratap98@users.noreply.github.com>
2023-02-10 15:36:43 +0530

    destimation

---
+++ zostel/src/Components/destinations/ExpDestinations.js
@@ -0,0 +1,42 @@
+// Explore Destinations
+import data from "../../demo.json";
+import { useState } from "react";
+
+
+
+
+function ExpDestination(){
+
+    let [item, setItem] = useState("")
+
+    console.log(data.Destinations);
+    return (
+        <>
+         <h1>Explore Destinations</h1>
+        <div className="des" style={{display: "flex", flexWrap:"wrap", justifyContent:"center", alignItems:"center"}}>
+           
+            {
+                data.Destinations.map((e) => {
+                    return (
+                        <div style={{
+                            backgroundImage: `url(${e.img})`, 
+                            width: "300px",
+                            height: "300px",
+                            margin: "8px",
+                            display: "flex",
+                            padding: "10px",
+                            backgroundSize: "cover",
+                            borderRadius: "10px"                    
+                            }}>
+                            {e.name}
+                        </div>
+                        // <img src={e.img}></img>
+                    )
+                })
+            }
+        </div>
+        </>
+    )
+    
+}
+export default ExpDestination;
