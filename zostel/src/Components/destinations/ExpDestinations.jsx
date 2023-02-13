4831bc0c9da78987fb0d6e8e3425c942b5e28fd7
krishnapratap98 <117529882+krishnapratap98@users.noreply.github.com>
2023-02-13 13:26:40 +0530

    Add files via upload

--- zostel/src/Components/destinations/ExpDestinations.jsx
+++ zostel/src/Components/destinations/ExpDestinations.jsx
@@ -1,1 +1,43 @@
-// Explore Destinations
+// Explore Destinations// Explore Destinations
+import data from "../../demo.json";
+import { useState } from "react";
+
+
+
+
+function ExpDestination(){
+
+    let [item, setItem] = useState("")
+
+    console.log(data.Destinations);
+    return (
+        <>
+         <h1>Explore <span style={{color: "#f15824"}}> Destinations</span></h1>
+         <p style={{textAlign: "left",paddingLeft: "80px", margin:"20px 0px"}}>Home <span><i class="fa-solid fa-arrow-right"></i></span> Destinations</p>
+        <div className="des" style={{display: "flex", flexWrap:"wrap", justifyContent:"center", alignItems:"center"}}>
+           
+            {
+                data.Destinations.map((e) => {
+                    return (
+                        <div style={{
+                            backgroundImage: `url(${e.img})`, 
+                            width: "300px",
+                            height: "300px",
+                            margin: "8px",
+                            display: "flex",
+                            padding: "10px",
+                            backgroundSize: "cover",
+                            borderRadius: "10px"                    
+                            }}>
+                            <h3 style={{color:"white", position:"relative", top:"90%"}}>{e.name}</h3>
+                        </div>
+                        // <img src={e.img}></img>
+                    )
+                })
+            }
+        </div>
+        </>
+    )
+    
+}
+export default ExpDestination;
