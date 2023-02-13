9d10d6e1f92f084277143df77e1d3c083ddd4aa8
rachna rajak <113829584+rachnarajaka040@users.noreply.github.com>
2023-02-13 14:13:11 +0530

    zostel

--- zostel/src/Components/Policies.jsx
+++ zostel/src/Components/Policies.jsx
@@ -1,1 +1,24 @@
 // Common for HotelPage, Payment page
+// Common for HotelPage, Payment page
+function Policies({policies})
+{
+    return <>
+        <div className="policies">
+            <h2>Property Policy</h2>
+            <ul>
+                {policies.property.map(e=>{
+                    return <li>{e}</li>
+                })}
+            </ul>
+            <h2>Cancellation Policy</h2>
+            <ul>
+                {policies.cancellation.map(e=>{
+                    return <li>{e}</li>
+                })}
+            </ul>
+            <p>For any other queries, please reach out to us at reservations@zostel.com.</p>
+        </div>
+
+     </>
+}
+export default Policies;
